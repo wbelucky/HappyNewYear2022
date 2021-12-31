ddc349a3e4ffecbb6de2057d5e577b3a3098b7f0
wbelucky <39439193+WBelucky@users.noreply.github.com>
2021-12-31 14:35:00 +0900

    update

--- docs/main.c39d6dcf.js
+++ docs/main.c39d6dcf.js
@@ -50002,7 +50002,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40079" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41079" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
